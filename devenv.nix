9a3b2070252edfe1c74a18ea26ccf63d562b301c
Dmitry Akatov <dmitry.akatov@protonmail.com>
2023-12-16 16:14:49 +0200

    cleanup

--- devenv.nix
+++ devenv.nix
@@ -2,7 +2,6 @@
 
 {
   # https://devenv.sh/basics/
-  env.GREET = "devenv";
 
   packages = with pkgs; [
     git
@@ -11,27 +10,15 @@
     stack
     haskellPackages.hoogle
     haskellPackages.hasktags
+    haskellPackages.haskdogs
     haskell.compiler.ghc945
     haskell.packages.ghc945.haskell-language-server
     haskell.packages.ghc945.hlint
   ];
-
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = "echo hello from $GREET";
 
   enterShell = ''
     Hello fellow hacker
     ghc --version
   '';
 
-  # https://devenv.sh/languages/
-  # languages.nix.enable = true;
-
-  # https://devenv.sh/pre-commit-hooks/
-  pre-commit.hooks.shellcheck.enable = true;
-
-  # https://devenv.sh/processes/
-  # processes.ping.exec = "ping example.com";
-
-  # See full reference at https://devenv.sh/reference/options/
 }
